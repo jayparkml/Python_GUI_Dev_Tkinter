cbecf0bda8203b09bc0486077b6bcafbb5e8571f
mintchococookie <parksungju90@gmail.com>
2020-08-06 13:30:47 -0500

    2

--- Ch06/01_mypack.py
+++ Ch06/01_mypack.py
@@ -8,11 +8,20 @@ from tkinter import ttk
 root = Tk()
 
 # code goes here
+label = ttk.Label(root, text="Hello, Tkinter!",
+          background = "yellow")
+label.pack(side= LEFT, anchor = 'nw')
+
+print(label)
+
 ttk.Label(root, text="Hello, Tkinter!",
-          background = "yellow").pack(fill=BOTH, expand=True)
+          background = "blue").pack(side= LEFT, padx=10, pady=10)
 ttk.Label(root, text="Hello, Tkinter!",
-          background = "blue").pack(fill=BOTH)
-ttk.Label(root, text="Hello, Tkinter!",
-          background = "green").pack(fill=BOTH, expand=True)          
+          background = "green").pack(side= LEFT, ipadx = 10, ipady = 10)          
+
+for widget in root.pack_slaves():
+    widget.pack_configure(fill=BOTH, expand=True)
+    print(widget.pack_info())#properties
+
 
 root.mainloop()
